f613d941a4baec4ce2771c19ca51fc4146d10c7b
payalbal <payalbal@hotmail.com>
2022-06-24 08:17:30 +0000

    fixed untracked files

--- scripts/archive/raster_uniquevals.py
+++
@@ -1,7 +0,0 @@
-from osgeo import gdal
-import numpy as np
-
-def raster_uniquevals(file):
-  ds = gdal.Open(file)
-  values = np.unique(np.array(ds.GetRasterBand(1).ReadAsArray()))
-  return values
